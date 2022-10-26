09debd28f7a621d334c04f1b364c9f71d81d573c
bhavanirajgit <116632632+bhavanirajgit@users.noreply.github.com>
2022-10-26 22:27:27 +0530

    Update EmployeeDetails.java

--- src/org/gittest/EmployeeDetails.java
+++ src/org/gittest/EmployeeDetails.java
@@ -1,16 +1,20 @@
-package org.gittest;
+ package org.gittest;
 
-public class EmployeeDetails {
-private void empName() {
-  System.out.println("sajan"); 
-  }
-private void empNumber() {
-	System.out.println("9876543210");
-}
+ public class EmployeeDetails {
+    
+	private void empName() {
+              System.out.println("sajan"); 
+       }
+       
+	private void empNumber() {
+	      System.out.println("9876543210");
+       }
 
-public static void main(String[] args) {
-EmployeeDetails e = new EmployeeDetails();	
-e.empName();
-e.empNumber();
-}
-}
+ public static void main(String[] args) {
+             
+	EmployeeDetails e = new EmployeeDetails();	
+        e.empName();
+        e.empNumber();
+     
+       }
+ }
